dbda1963d7566ac5d849a7ba90af5f443deb38b7
virkill <arif@tokenomy.com>
2019-02-26 10:29:08 +0700

    change prod log level to warn

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ config :avatar_api, AvatarApiWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :warn
 
 # ## SSL Support
 #
